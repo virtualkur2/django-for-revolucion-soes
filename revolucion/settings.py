d182ea54dfe0ed40eed591ddb2249ea885c4b913
Mauricio Contreras <virtual.kur2@gmail.com>
2021-03-31 16:14:59 +0200

    Se elemina SECRET_KEY

--- revolucion/settings.py
+++ revolucion/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#&tg-)s*v@&r_h*ouc=$1_vy%9kt*y*#mw#!*0ka1w78g=9hke'
+# SECRET_KEY = 'IMPLEMENT_YOUR_OWN'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
